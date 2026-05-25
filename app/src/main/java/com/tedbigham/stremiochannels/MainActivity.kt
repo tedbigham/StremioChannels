d28d4c44d37b73e7db35ab32826468300dcacc48
Ted Bigham <tedbigham@yahoo.com>
2026-05-25 15:07:35 -0400

    Remove hello world

--- app/src/main/java/com/tedbigham/stremiochannels/MainActivity.kt
+++ app/src/main/java/com/tedbigham/stremiochannels/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.tedbigham.stremiochannels
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
+        Log.i(TAG, "Launcher setup entry opened; scheduling TMDB refresh work")
         TmdbRefreshScheduler.schedulePeriodicRefresh(this)
         TmdbRefreshScheduler.scheduleStartupRefresh(this)
+        finish()
+    }
+
+    private companion object {
+        private const val TAG = "MainActivity"
     }
 }
